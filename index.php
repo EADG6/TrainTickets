0340f22049b9541921bdc6fafe6d23abf21b13c9
MarshallCN <collapselzh@live.com>
2017-04-17 15:46:00 +0800

    create basic file structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	//Start the session
+	session_start();
+	date_default_timezone_set('PRC');
+	//include files
+	require "inc/db.php";
+	include "inc/header.php";
+	include "inc/nav.php";
+	//Main Pages
+	
+	
+	include "inc/footer.php";
+?>
