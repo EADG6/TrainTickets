51f7fb42ed45384d3b328826e31cece1fa4c280b
qq398935137 <398935137@qq.com>
2017-05-08 16:17:52 +0800

    update ticket_all page form

--- app/ticket_all.php
+++ app/ticket_all.php
@@ -1,182 +1,246 @@
- 
-	<div class='form-group'>
-		<div class="panel-group" id="panel-1">
-					  <div class="panel" style="background:#4c85db;color:#fff" onclick='location.href="index.php"'><div class="panel-heading icona"><img src=''> 	<div class="py-5">
-							<div class="container">
-							  <div class="row">
-								<div class="col-md-6">
-								  <h4 class="display-2">Start City</h4>
+<div class='form-group'>
+	<div class="panel-group" id="panel-1">
+	  <div class="panel panel-primary">
+	  <div class="panel-heading icona">	
+		<div class="container">
+		  <div class="row">
+			<div class="col-md-6">
+				<h4 class="text-center">Start City</h4>
+			</div>
+			<div class="col-md-6">
+				<h4 class="text-center">End City</h4>
+			</div>
+		  </div>
+		</div>
+		</div>
+	  </div>
+		<div class="panel panel-default" style='margin-top:0px;'>
+			<div class="panel-heading" onclick="$('#ticket1')[0].click()">
+				<a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-1" id="ticket1" href="#panel-element-1" style="text-decoration:none;">
+					<div class="col-md-6 text-center">Chengdu</div>
+					<div class="col-md-3 text-right">Beijing</div>
+				</a><span class='caret'></span>
+			</div>
+				<div id="panel-element-1" class="panel-collapse collapse">
+					<div>
+					<a id="modal-914981" href="#modal-container-914981" role="button" class="btn" data-toggle="modal">May 6</a>
+						<div class="modal fade" id="modal-container-914981" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						 <div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+										<h4 class="modal-title" id="myModalLabel">
+										Ticket Management
+										</h4>
 								</div>
-								<div class="col-md-6">
-								  <h4 class="display-2">End City</h4>
-								</div>
-							  </div>
+							<div class="modal-body">
+									<table class ='table table table-striped'>
+										<thead>
+											<th colspan='9'>Ticket Information</th>
+											<tr>
+												<th>TrianID</th>
+												<th>Time</th>
+												<th>Hours</th>
+												<th>Price</th>
+												<th>Customer</th>
+												<th>Seats</th>
+												<th>Carriage</th>
+												<th>
+													Operation <a href='javascript:void(0);' onclick="$('#helptip1').toggle()" class="glyphicon glyphicon-question-sign icona"></a>
+												</th>
+											</tr>
+										</thead>
+										<tbody>
+												<th>DH389</th>
+												<th>8:00-20:00</th>
+												<th>12h</th>
+												<th>300￥</th>
+												<th>John</th>
+												<th>Stand</th>
+												<th>1</th>
+												<th>
+													EDIT  DELETE
+												</th>	
+												
+												
+										</tbody>
+									</table>
 							</div>
-						  </div></div>
-					  </div>
-						<div class="panel panel-default" style='margin-top:0px;'>
-							<div class="panel-heading" onclick="$('#ticket1')[0].click()">
-								<a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-1" id="ticket1" href="#panel-element-1" style="text-decoration:none;">
-									Chengdu--Shanghai
-								</a><span class="caret"></span>
+							<div class="modal-footer">
+								 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary">Save</button>
 							</div>
-								<div id="panel-element-1" class="panel-collapse collapse">
-									<div>
-									<a id="modal-914981" href="#modal-container-914981" role="button" class="btn" data-toggle="modal">May 6</a>
-										<div class="modal fade" id="modal-container-914981" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-										 <div class="modal-dialog">
-											<div class="modal-content">
-												<div class="modal-header">
-													<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-														<h4 class="modal-title" id="myModalLabel">
-														标题
-														</h4>
-												</div>
-											<div class="modal-body">
-													<table class ='table table table-striped'>
-														<thead>
-															<th colspan='9'>Ticket Information</th>
-															<tr>
-																<th>TrianID</th>
-																<th>Time</th>
-																<th>Hours</th>
-																<th>Price</th>
-																<th>Customer</th>
-																<th>Seats</th>
-																<th>Carriage</th>
-																<th>
-																	Operation <a href='javascript:void(0);' onclick="$('#helptip1').toggle()" class="glyphicon glyphicon-question-sign icona"></a>
-																</th>
-															</tr>
-														</thead>
-														<tbody>
-																<th>DH389</th>
-																<th>8:00-20:00</th>
-																<th>12h</th>
-																<th>300￥</th>
-																<th>John</th>
-																<th>Stand</th>
-																<th>1</th>
-																<th>
-																	Operation <a href='javascript:void(0);' onclick="$('#helptip1').toggle()" class="glyphicon glyphicon-question-sign icona"></a>
-																</th>	
-																
-																
-														</tbody>
-													</table>
-											</div>
-											<div class="modal-footer">
-												 <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button> <button type="button" class="btn btn-primary">保存</button>
-											</div>
-											</div>					
-										   </div>				
-										</div>
-									</div>	
-									<div>
-									<a id="modal-914982" href="#modal-container-914982" role="button" class="btn" data-toggle="modal">May 7</a>
-																			<div class="modal fade" id="modal-container-914982" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-										 <div class="modal-dialog">
-											<div class="modal-content">
-												<div class="modal-header">
-													<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-														<h4 class="modal-title" id="myModalLabel">
-														标haha 
-														</h4>
-												</div>
-											<div class="modal-body">
-													<table class ='table table table-striped'>
-														<thead>
-															<th colspan='9'>Ticket Information</th>
-															<tr>
-																<th>TrianID</th>
-																<th>Time</th>
-																<th>Hours</th>
-																<th>Price</th>
-																<th>Customer</th>
-																<th>Seats</th>
-																<th>Carriage</th>
-																<th>
-																	Operation <a href='javascript:void(0);' onclick="$('#helptip1').toggle()" class="glyphicon glyphicon-question-sign icona"></a>
-																</th>
-															</tr>
-														</thead>
-														<tbody>
-																<th>DH389</th>
-																<th>8:00-20:00</th>
-																<th>12h</th>
-																<th>300￥</th>
-																<th>John</th>
-																<th>Stand</th>
-																<th>1</th>
-																<th>
-																	Operation <a href='javascript:void(0);' onclick="$('#helptip1').toggle()" class="glyphicon glyphicon-question-sign icona"></a>
-																</th>	
-																
-																
-														</tbody>
-													</table>
-											</div>
-											<div class="modal-footer">
-												 <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button> <button type="button" class="btn btn-primary">保存</button>
-											</div>
-											</div>					
-										   </div>				
-										</div>
-									</div>									
-								</div>
-
-													
+							</div>					
+						   </div>				
 						</div>
+					</div>	
+					<div>
+					<a id="modal-914982" href="#modal-container-914982" role="button" class="btn" data-toggle="modal">May 7</a>
+															<div class="modal fade" id="modal-container-914982" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						 <div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+										<h4 class="modal-title" id="myModalLabel">
+										Ticket Management
+										</h4>
+								</div>
+							<div class="modal-body">
+									<table class ='table table table-striped'>
+										<thead>
+											<th colspan='9'>Ticket Information</th>
+											<tr>
+												<th>TrianID</th>
+												<th>Time</th>
+												<th>Hours</th>
+												<th>Price</th>
+												<th>Customer</th>
+												<th>Seats</th>
+												<th>Carriage</th>
+												<th>
+													Operation <a href='javascript:void(0);' onclick="$('#helptip1').toggle()" class="glyphicon glyphicon-question-sign icona"></a>
+												</th>
+											</tr>
+										</thead>
+										<tbody>
+												<th>DH389</th>
+												<th>8:00-20:00</th>
+												<th>12h</th>
+												<th>300￥</th>
+												<th>John</th>
+												<th>Stand</th>
+												<th>1</th>
+												<th>
+													EDIT  DELETE
+												</th>	
+												
+												
+										</tbody>
+									</table>
+							</div>
+							<div class="modal-footer">
+								 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary">Save</button>
+							</div>
+							</div>					
+						   </div>				
+						</div>
+					</div>									
+				</div>									
+		</div>
+		<div class="panel panel-default" style='margin-top:0px;'>
 							<div class="panel-heading" onclick="$('#train1')[0].click()">
-								<a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-1" id="train1" href="#panel-element-2" style="text-decoration:none;">
-									Beijing--Yunnan
+								<a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-2" id="train1" href="#panel-element-2" style="text-decoration:none;">
+									<div class="col-md-6 text-center">Wuhan</div>
+									<div class="col-md-3 text-right">Sichuan</div>
 								</a><span class="caret"></span>
 							</div>
 							<div id="panel-element-2" class="panel-collapse collapse">
-								<a href="index.php?page=ticket&action=new" class="panel-legend">
-									<div class="panel-body">May 5</div>
-								</a>
-								<a href="index.php?page=ticket&action=all" class="panel-legend">
-									<div class="panel-body">May 6</div>
-								</a>
-								<a href="index.php?page=ticket&action=all" class="panel-legend">
-									<div class="panel-body">May 7</div>
-								</a>
+					<div>
+					<a id="modal-914983" href="#modal-container-914983" role="button" class="btn" data-toggle="modal">May 6</a>
+						<div class="modal fade" id="modal-container-914983" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						 <div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+										<h4 class="modal-title" id="myModalLabel">
+										Ticket Management
+										</h4>
+								</div>
+							<div class="modal-body">
+									<table class ='table table table-striped'>
+										<thead>
+											<th colspan='9'>Ticket Information</th>
+											<tr>
+												<th>TrianID</th>
+												<th>Time</th>
+												<th>Hours</th>
+												<th>Price</th>
+												<th>Customer</th>
+												<th>Seats</th>
+												<th>Carriage</th>
+												<th>
+													Operation <a href='javascript:void(0);' onclick="$('#helptip1').toggle()" class="glyphicon glyphicon-question-sign icona"></a>
+												</th>
+											</tr>
+										</thead>
+										<tbody>
+												<th>DH389</th>
+												<th>8:00-20:00</th>
+												<th>12h</th>
+												<th>300￥</th>
+												<th>John</th>
+												<th>Stand</th>
+												<th>1</th>
+												<th>
+													EDIT  DELETE
+												</th>	
+												
+												
+										</tbody>
+									</table>
 							</div>
-							<div class="panel-heading" onclick="$('#customer1')[0].click()">
-								<a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-1" id="customer1" href="#panel-element-3" style="text-decoration:none;">
-									Wuhan--Sichuan
-								</a><span class="caret"></span>
+							<div class="modal-footer">
+								 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary">Save</button>
 							</div>
-							<div id="panel-element-3" class="panel-collapse collapse">
-								<a href="index.php?page=ticket&action=new" class="panel-legend">
-									<div class="panel-body">May 5</div>
-								</a>
-								<a href="index.php?page=ticket&action=all" class="panel-legend">
-									<div class="panel-body">May 6</div>
-								</a>
-								<a href="index.php?page=ticket&action=all" class="panel-legend">
-									<div class="panel-body">May 7</div>
-								</a>
-							</div>
-							<div class="panel-heading" onclick="$('#staff1')[0].click()">
-								<a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-1" id="staff1" href="#panel-element-4" style="text-decoration:none;">
-									Hainan--Guangzhou
-								</a><span class="caret"></span>
-							</div>
-							<div id="panel-element-4" class="panel-collapse collapse">
-								<a href="index.php?page=ticket&action=new" class="panel-legend">
-									<div class="panel-body">May 5</div>
-								</a>
-								<a href="index.php?page=ticket&action=all" class="panel-legend">
-									<div class="panel-body">May 6</div>
-								</a>
-								<a href="index.php?page=ticket&action=all" class="panel-legend">
-									<div class="panel-body">May 7</div>
-								</a>
-							</div>
+							</div>					
+						   </div>				
 						</div>
+					</div>	
+					<div>
+					<a id="modal-914982" href="#modal-container-914982" role="button" class="btn" data-toggle="modal">May 7</a>
+															<div class="modal fade" id="modal-container-914982" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						 <div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+										<h4 class="modal-title" id="myModalLabel">
+										Ticket Management
+										</h4>
+								</div>
+							<div class="modal-body">
+									<table class ='table table table-striped'>
+										<thead>
+											<th colspan='9'>Ticket Information</th>
+											<tr>
+												<th>TrianID</th>
+												<th>Time</th>
+												<th>Hours</th>
+												<th>Price</th>
+												<th>Customer</th>
+												<th>Seats</th>
+												<th>Carriage</th>
+												<th>
+													Operation <a href='javascript:void(0);' onclick="$('#helptip1').toggle()" class="glyphicon glyphicon-question-sign icona"></a>
+												</th>
+											</tr>
+										</thead>
+										<tbody>
+												<th>DH389</th>
+												<th>8:00-20:00</th>
+												<th>12h</th>
+												<th>300￥</th>
+												<th>John</th>
+												<th>Stand</th>
+												<th>1</th>
+												<th>
+													EDIT  DELETE
+												</th>	
+												
+												
+										</tbody>
+									</table>
+							</div>
+							<div class="modal-footer">
+								 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary">Save</button>
+							</div>
+							</div>					
+						   </div>				
+						</div>
+					</div>									
+				</div>									
+							</div>
 	</div>
+</div>
+
 		
 
 
