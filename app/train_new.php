5eddeef4d646354ea124b33b9a58b3ac6af701e4
leo-lizhuoxuan <437792560@qq.com>
2017-05-27 20:49:17 +0800

    update function for train_new page

--- app/train_new.php
+++ app/train_new.php
@@ -6,7 +6,7 @@
     </div>
     <div class="form-group col-md-6">
         <label>Train Type: <span id='trainSelected'></span></label>
-			<input type="hidden" name="tid" id="tid"/>
+			<input type="hidden" name="tid" id="tid" required/>
 			<select class="form-control" name='ttype' onchange='trainTime()' required>		
 				<?php
 					$sql_traintype = "SELECT * FROM train_type";
@@ -47,19 +47,19 @@
     <div class="form-group col-md-6">
         <div class="form-group col-md-6">
             <label>Start Time :</label>
-            <input class="form-control" type="time" name="stime">
+            <input class="form-control" type="time" name="stime" required>
         </div>
         <div class="form-group col-md-6">
             <label>hours :</label>
-            <input class="form-control" type="text" name="hours">
+            <input class="form-control" type="text" name="hours" required>
         </div>
     </div>
     <div class="form-group col-md-12">
         <label class=>Seat Level :</label>
     </div>
-    <div class="form-group col-md-4">
-        <label>Seat Cariage</label>
-            <select name="seatca" class="form-control">
+    <div class="form-group col-md-3">
+        <label>High Seat Cariage</label>
+            <select name="hseatca" class="form-control">
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -74,7 +74,24 @@
                 <option>12</option>
             </select>
     </div>
-    <div class="form-group col-md-4">
+    <div class="form-group col-md-3">
+        <label>Slow Seat Cariage</label>
+            <select name="sseatca" class="form-control">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>
+            </select>
+    </div>
+    <div class="form-group col-md-3">
         <label>Hard Sleeper Cariage</label>
         <select name="hsleepca" class="form-control">
             <option>1</option>
@@ -91,7 +108,7 @@
             <option>12</option>
         </select>
     </div>
-    <div class="form-group col-md-4">
+    <div class="form-group col-md-3">
         <label>Soft Sleeper Cariage</label>
         <select name="ssleepca" class="form-control">
             <option>1</option>
@@ -123,14 +140,29 @@
             $gotime = $_POST['stime'];
             $hours = $_POST['hours'];
             $train_type_id = $_POST['ttype'];
-        if($name==""|| $start_city_id=="" || $end_city_id=="" || $gotime=="" || $hours=="" || $train_type_id=="") {  
+            $hseatca = $_POST['hseatca'];
+            $sseatca = $_POST['sseatca'];
+            $hsleepca = $_POST['hsleepca'];
+            $ssleepca = $_POST['ssleepca'];
+        if($start_city_id=="" || $end_city_id==""|| $train_type_id=="") {  
 		echo"<script type='text/javascript'>alert('write all the information');location='index.php?page=ticket&action=new';  
             </script>";
         }else{
             $sql_newtrain = "INSERT INTO train VALUES('','$name','$start_city_id','$end_city_id','$gotime','$hours','$train_type_id')"; 
             echo $sql_newtrain;
-			       // $mysql->query($sql_newtrain);
-					//echo"<script type='text/javascript'>alert('Add New Train Successful');location='index.php?page=train&action=all';</script>"; 
+            $mysql->query($sql_newtrain);
+            $query="SELECT LAST_INSERT_ID()";
+            $result=mysql_query($query);
+            $rows=mysql_fetch_row($result);
+            $sql_hseatca = "INSERT INTO cariage VALUES('','2','$rows','$hseatca')"; 
+            $mysql->query($sql_hseatca);
+            $sql_sseatca = "INSERT INTO cariage VALUES('','4','$rows','$sseatca')"; 
+            $mysql->query($sql_sseatca);
+            $sql_hsleepca = "INSERT INTO cariage VALUES('','5','$rows','$hsleepca')"; 
+            $mysql->query($sql_hsleepca);
+            $sql_ssleepca = "INSERT INTO cariage VALUES('','6','$rows','$ssleepca')"; 
+            $mysql->query($sql_ssleepca);
+            echo"<script type='text/javascript'>alert('Add New Train Successful');location='index.php?page=train&action=all';</script>"; 
         }
     }
 ?>
