f427427c44cd816087020da887cc10735b8ee39a
Johnny <597401439@qq.com>
2017-05-30 20:26:33 +0800

    staff_new_js
    move the js code from staff_new.php to staff_new.js

--- app/staff_new.php
+++ app/staff_new.php
@@ -50,48 +50,7 @@
         <button type="submit" class="btn btn-primary btn-block" name='newstaff' disabled>Submit</button>
     </div>
 </form>
-<script>
-	/* use ajax to Check username if exist */
-	function checkNewName(){
-		$('[name="username"]').val($('[name="username"]').val().replace(" ",""))
-		if($('[name="username"]').val().length>0){
-			$.ajax({
-				url:'ajax.php',
-				data:{"usercheck":encodeURI(encodeURI($('[name="username"]').val()))},
-				success:function(data){
-				setTimeout(function(){
-					if(data.used=='used'){
-						$('[name="username"]').attr('class','form-control alert-danger')
-						$('[name="username"]').next().attr('class','seepwd alert-danger')
-						$('[name="username"]').next().children('i').attr('class','fa fa-close')
-						$('[name="newstaff"]').attr('disabled',true)
-					}else if(data.used=='ok'){
-						$('[name="username"]').attr('class','form-control alert-success')
-						$('[name="username"]').next().attr('class','seepwd alert-success')
-						$('[name="username"]').next().children('i').attr('class','fa fa-check')
-						$('[name="newstaff"]').attr('disabled',false)
-					}else if(data.used=='empty'){
-						$('[name="username"]').attr('class','form-control')
-						$('[name="username"]').next().attr('class','seepwd hidden')
-						$('[name="newstaff"]').attr('disabled',true)
-					}
-				},500)
-				},
-				type:'POST',
-				dataType:'json',
-				beforeSend:function(){
-					$('[name="username"]').next().attr('class','seepwd btn-warning')
-					$('[name="username"]').next().children('i').attr('class','fa fa-refresh fa-spin')
-					$('[name="newstaff"]').attr('disabled',true)
-				}
-			});
-		}else{
-			$('[name="username"]').attr('class','form-control')
-			$('[name="username"]').next().attr('class','seepwd hidden')
-			$('[name="newstaff"]').attr('disabled',true)
-		}
-	}
-</script>
+<script src='static/js/staff_new.js'></script>
 <?php
 	if(isset($_POST['newstaff'])){
 		$username = inputCheck($_POST['username']);
